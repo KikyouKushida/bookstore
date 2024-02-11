715cdf5519669af0c9e3b96e200f7e48420df415
KikyouKushida <105808948+KikyouKushida@users.noreply.github.com>
2024-02-11 16:17:06 +0800

    Fix the bug when inheriting the previous file.

--- block.cpp
+++ block.cpp
@@ -1,13 +1,10 @@
 #include <bits/stdc++.h>
 
-const int B = 400, L = 17;
-
-int n;
-
+const int B = 500, L = 17;
 char space = ' ';
-
-std::fstream file;
-std::string file_name = "f.txt";
+std::fstream file, leif;
+std::ifstream ifile;
+std::string file_name = "f.txt", leif_name = "g.txt";
 
 struct par{
   std::string s;
@@ -70,7 +67,7 @@ void simple_read(std::string &tmp){
 }
 
 void simple_read(int &tmp){
-  char c; tmp = 0;
+  tmp = 0;
   file.read(reinterpret_cast<char *>(&tmp), sizeof(int));
   return ;
 }
@@ -91,116 +88,174 @@ void delete_any(int p, int q){
   return ;
 }
 
-int main(){
-  file.open(file_name, std::ios::in | std::ios::out | std::ios::binary);
-  if(!file){
+void initialize(){
+  ifile.open(file_name);
+  if(!ifile){
     file.open(file_name, std::ios::out | std::ios::binary);
     file.close();
+    par ST("", 0);
+    ST.write(0);
+    head[cnt = 1] = 0, siz[1] = 1;
+    nownew = L * B;
   }
-  else file.close();
-  par ST("", 0);
-  ST.write(0);
-  head[cnt = 1] = 0, siz[1] = 1;
+  else {
+    ifile.close();
+    leif.open(leif_name, std::ios::in | std::ios::out | std::ios::binary);
+    leif.seekg(0, std::ios::beg);
+    leif.read(reinterpret_cast<char *>(&cnt), sizeof(int));
+    leif.read(reinterpret_cast<char *>(&nownew), sizeof(int));
+    for(int i = 1; i <= cnt; ++i){
+      leif.read(reinterpret_cast<char *>(&head[i]), sizeof(int));
+      leif.read(reinterpret_cast<char *>(&siz[i]), sizeof(int));
+    }
+    leif.close();
+  }
+  return ;
+}
+
+void store(){
+  leif.open(leif_name, std::ios::out | std::ios::binary);
+  leif.seekp(0, std::ios::beg);
+  leif.write(reinterpret_cast<char *>(&cnt), sizeof(int));
+  leif.write(reinterpret_cast<char *>(&nownew), sizeof(int));
+  for(int i = 1; i <= cnt; ++i){
+    leif.write(reinterpret_cast<char *>(&head[i]), sizeof(int));
+    leif.write(reinterpret_cast<char *>(&siz[i]), sizeof(int));
+  }
+  leif.close();
+  return ;
+}
+
+void insert(std::string str, int value){
+  int tmpv; std::string tmps;
+  par cur(str, value);
+  int choose = -1;
+  for(int j = 1; j <= cnt; ++j){
+    par now; read(head[j], now);
+    if(now == cur){
+      choose = -1;
+      break;
+    }
+    else if(now < cur) choose = j;
+    else break;
+  }
+  if(choose == -1) continue;
+  file.open(file_name, std::ios::in | std::ios::out | std::ios::binary);
+  int size = siz[choose], put = 0; file.seekg(head[choose] * sizeof(int), std::ios::beg);
+  for(int i = 1; i <= size; ++i){
+    char c; temp[i].s = "";
+    for(int j = 0; j < 64; ++j){
+      file.read(reinterpret_cast<char *>(&c), sizeof(char));
+      if(c != ' ') temp[i].s += c;
+    }
+    file.read(reinterpret_cast<char *>(&(temp[i].val)), sizeof(int));
+    if(temp[i] == cur) put = -1;
+    else if(temp[i] < cur) put = i;
+  }
+  if(put == -1){
+    file.close();
+    continue;
+  } 
+  for(int i = size; i >= put + 1; --i) temp[i + 1] = temp[i];
+  temp[put + 1] = cur; siz[choose] += 1; size += 1;
+  if(siz[choose] <= B){
+    file.seekp(head[choose] * sizeof(int), std::ios::beg);
+    for(int i = 1; i <= size; ++i) temp[i].simple_write();
+  }
+  else {
+    siz[choose] >>= 1; siz[++cnt] = size - siz[choose];
+    head[cnt] = nownew; nownew += L * B;
+    file.seekp(head[choose] * sizeof(int), std::ios::beg);
+    for(int i = 1; i <= siz[choose]; ++i) temp[i].simple_write();
+    file.seekp(head[cnt] * sizeof(int), std::ios::beg);
+    for(int i = 1; i <= siz[cnt]; ++i) temp[siz[choose] + i].simple_write();
+    for(int i = cnt - 1; i >= choose + 1; --i) std::swap(head[i], head[i + 1]), std::swap(siz[i], siz[i + 1]); 
+  }
+  file.close();
+  return ;
+}
+
+void find(std::string str){
+  int status = 0, start = 0, hav = 0, tmpv;
+  std::string tmps;
+  file.open(file_name, std::ios::in | std::ios::out | std::ios::binary);
+  for(int i = 1; i <= cnt; ++i){
+    file.seekg(head[i] * sizeof(int), std::ios::beg);
+    simple_read(tmps);
+    if(tmps < str) start = i;
+    else break;
+  }
+  assert(start != 0);
+  for(int i = start; i <= cnt; ++i){
+    if(status == 2) break;
+    file.seekg(head[i] * sizeof(int), std::ios::beg);
+    for(int j = 1; j <= siz[i]; ++j){
+      simple_read(tmps);
+      simple_read(tmpv);
+      if(tmps == str){
+        hav = 1;
+        if(status == 0) std::cout << tmpv, status = 1;
+        else std::cout << " " << tmpv;
+      }
+      if(tmps > str){
+        status = 2;
+        break;
+      }
+    }
+  }
+  if(hav == 0) std::cout << "null" << "\n";
+  else std::cout << "\n";
+  file.close();
+  return ;
+}
+
+void erase(std::string str, int value){
+  std::string tmps; int tmpv;
+  par cur(str, value);         
+  int choose = -1;
+  for(int j = 1; j <= cnt; ++j){
+    par now; read(head[j], now);
+    if(now == cur){
+      choose = -1;
+      file.open(file_name, std::ios::in | std::ios::out | std::ios::binary);
+      delete_any(j, 1);
+      file.close();
+      break;
+    }
+    else if(now < cur) choose = j;
+  }
+  if(choose == -1) continue;
+  file.open(file_name, std::ios::in | std::ios::out | std::ios::binary);
+  file.seekg(head[choose] * sizeof(int), std::ios::beg);
+  for(int i = 1; i <= siz[choose]; ++i){
+    simple_read(tmps);
+    simple_read(tmpv);
+    if(tmps == str && tmpv == value){
+      delete_any(choose, i);
+      break;
+    }
+  }
+  file.close();
+  return ;
+}
+
+int main(){
+  initialize();
   //std::ios::sync_with_stdio(false);
   //std::cin.tie(0), std::cout.tie(0);
-  nownew = L * B;
-  std::cin >> n;
+  int n; std::cin >> n;
   for(int T = 1; T <= n; ++T){
     std::string type, str, tmps; 
     std::cin >> type >> str;
     int value, tmpv;
-    if(type != "find") std::cin >> value;
-    if(type == "find"){
-      int status = 0, start, hav = 0;
-      file.open(file_name, std::ios::in | std::ios::out | std::ios::binary);
-      for(int i = 1; i <= cnt; ++i){
-        file.seekg(head[i] * sizeof(int), std::ios::beg);
-        simple_read(tmps);
-        if(tmps < str) start = i;
-        else break;
-      }
-      for(int i = start; i <= cnt; ++i){
-        if(status == 2) break;
-        file.seekg(head[i] * sizeof(int), std::ios::beg);
-        for(int j = 1; j <= siz[i]; ++j){
-          simple_read(tmps);
-          simple_read(tmpv);
-          if(tmps == str){
-            hav = 1;
-            if(status == 0) std::cout << tmpv, status = 1;
-            else std::cout << " " << tmpv;
-          }
-          if(tmps > str){
-            status = 2;
-            break;
-          }
-        }
-      }
-      if(hav == 0) std::cout << "null" << "\n";
-      else std::cout << "\n";
-      file.close();
-    }
+    if(type == "find") find(str);
     else if(type == "insert"){
-      par cur(str, value);
-      int choose = -1;
-      for(int j = 1; j <= cnt; ++j){
-        par now; read(head[j], now);
-        if(now < cur) choose = j;
-        else break;
-      }
-      assert(choose != -1);
-      file.open(file_name, std::ios::in | std::ios::out | std::ios::binary);
-      int size = siz[choose], put = 0; file.seekg(head[choose] * sizeof(int), std::ios::beg);
-      for(int i = 1; i <= size; ++i){
-        char c; temp[i].s = "";
-        for(int j = 0; j < 64; ++j){
-          file.read(reinterpret_cast<char *>(&c), sizeof(char));
-          if(c != ' ') temp[i].s += c;
-        }
-        file.read(reinterpret_cast<char *>(&(temp[i].val)), sizeof(int));
-        if(temp[i] < cur) put = i;
-      }
-      for(int i = size; i >= put + 1; --i) temp[i + 1] = temp[i];
-      temp[put + 1] = cur; siz[choose] += 1; size += 1;
-      if(siz[choose] <= B){
-        file.seekp(head[choose] * sizeof(int), std::ios::beg);
-        for(int i = 1; i <= size; ++i) temp[i].simple_write();
-      }
-      else {
-        siz[choose] >>= 1; siz[++cnt] = size - siz[choose];
-        head[cnt] = nownew; nownew += L * B;
-        file.seekp(head[choose] * sizeof(int), std::ios::beg);
-        for(int i = 1; i <= siz[choose]; ++i) temp[i].simple_write();
-        file.seekp(head[cnt] * sizeof(int), std::ios::beg);
-        for(int i = 1; i <= siz[cnt]; ++i) temp[siz[choose] + i].simple_write();
-        for(int i = cnt - 1; i >= choose + 1; --i) std::swap(head[i], head[i + 1]), std::swap(siz[i], siz[i + 1]); 
-      }
-      file.close();
+      std::cin >> value;
+      insert(str, value);
     }
     else {
-      par cur(str, value);
-      int choose = -1;
-      for(int j = 1; j <= cnt; ++j){
-        par now; read(head[j], now);
-        if(now == cur){
-          choose = -1;
-          delete_any(j, 1);
-          break;
-        }
-        else if(now < cur) choose = j;
-      }
-      if(choose == -1) continue;
-      file.open(file_name, std::ios::in | std::ios::out | std::ios::binary);
-      file.seekp(head[choose] * sizeof(int), std::ios::beg);
-      for(int i = 1; i <= siz[choose]; ++i){
-        simple_read(tmps);
-        simple_read(tmpv);
-        if(tmps == str && tmpv == value){
-          delete_any(choose, i);
-          break;
-        }
-      }
-      file.close();
+      std::cin >> value;
+      erase(str, value);
     }
   }
   return 0;
